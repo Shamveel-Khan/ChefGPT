3325c88e2addf8ebaa6138ccfb7d1028756c8533
Shamveel-Khan <shamveelkhilji@gmail.com>
2025-01-28 20:22:58 +0500

    fix No.6

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header id="top-header">
-            <img id="claude-pic" src="./src/assets/chef-claude-icon.png" alt="claude icon" />
+            <img id="claude-pic" src="./public/chef-claude-icon.png" alt="claude icon" />
             <h3 id="heading">
                 Chef GPT
             </h3>
