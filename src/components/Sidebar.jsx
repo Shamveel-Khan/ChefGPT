75a04d6038c06935173158bbb741103406d5f623
Shamveel-Khan <shamveelkhilji@gmail.com>
2025-01-29 14:23:55 +0500

    changed colors

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,18 +9,12 @@ const Sidebar = () => {
 
   return (
     <div>
-      {/* Hamburger Icon */}
       <div className={`hamburger${isOpen ? "open":""}`} onClick={toggleSidebar}>
         {isOpen ? "✕" : "☰"}
       </div>
-
-      {/* Sidebar */}
       <div className={`sidebar ${isOpen ? "open" : ""}`}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
+          <li>About The Developer</li>
         </ul>
       </div>
     </div>
