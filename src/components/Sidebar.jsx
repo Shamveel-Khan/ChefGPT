e6fe51dc850b10869df4e75170a23cd4d65fb6bd
Shamveel-Khan <shamveelkhilji@gmail.com>
2025-01-29 17:36:03 +0500

    sidebar part 1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,6 +16,20 @@ const Sidebar = () => {
         <ul>
           <li>About The Developer</li>
         </ul>
+        <br />
+        <div id="mypicHolder">
+          <img id="mypic" src="myPic.jpg" alt="hello" />
+        </div>
+        <p id="about">
+        Hi, I'm Shamveel Khan, a freshman at FAST Karachi, passionate about coding and problem-solving. I'm currently learning React.js and experimenting with Tailwind CSS to build modern web applications. Always eager to improve my skills, I love turning ideas into functional projects.
+
+Feel free to connect with me through the links below! 
+        </p>
+        <div id="iconHolder">
+          <img className="icons" src="github.svg" alt="github-icon" /> 
+          <img className="icons" src="instagram.svg" alt="github-icon" /> 
+          <img className="icons" src="linkedin.svg" alt="github-icon" />           
+        </div>
       </div>
     </div>
   );
